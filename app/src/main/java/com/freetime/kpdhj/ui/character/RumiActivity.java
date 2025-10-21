4bd6ee6c803ad12667ea011b69dd5ee442a683d7
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-10-21 13:04:43 +0200

    changed a little bit

--- app/src/main/java/com/freetime/kpdhj/ui/character/RumiActivity.java
+++ app/src/main/java/com/freetime/kpdhj/ui/character/RumiActivity.java
@@ -2,6 +2,7 @@ package com.freetime.kpdhj.ui.character;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +17,18 @@ public class RumiActivity extends AppCompatActivity {
         // Immersive fullscreen
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+
+        Button rumiMusicButton = findViewById(R.id.rumiMusicButton);
+        rumiMusicButton.setOnClickListener(v -> {
+            Intent intent = new Intent(RumiActivity.this, RumiMusicActivity.class);
+            startActivity(intent);
+        });
 
         Button rumiInfoButton = findViewById(R.id.rumiBackToCharactersButton);
         rumiInfoButton.setOnClickListener(v -> {
---
+++ app/src/main/java/com/freetime/kpdhj/ui/character/RumiMusicActivity.java
@@ -0,0 +1,5 @@
+package com.freetime.kpdhj.ui.character;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class RumiMusicActivity extends AppCompatActivity {
+}
