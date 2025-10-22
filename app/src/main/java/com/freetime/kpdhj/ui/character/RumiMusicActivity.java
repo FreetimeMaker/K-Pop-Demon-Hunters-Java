c8c2471feadd955945509123da7624e42e955301
FreetimeMaker <jamie.achatz@s-stb.ch>
2025-10-22 07:28:49 +0000

    added and changed

--- app/src/main/java/com/freetime/kpdhj/ui/character/RumiMusicActivity.java
+++ app/src/main/java/com/freetime/kpdhj/ui/character/RumiMusicActivity.java
@@ -2,4 +2,19 @@ package com.freetime.kpdhj.ui.character;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class RumiMusicActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rumi_music);
+
+        // Immersive fullscreen
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
 }
