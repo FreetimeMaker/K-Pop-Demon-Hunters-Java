cfd3a9f4293d764f9fb1b33e742b7fb305c100d7
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-10-27 18:57:32 +0100

    changed a little bit

--- app/src/main/java/com/freetime/kpdhj/ui/home/HomeFragment.java
+++ app/src/main/java/com/freetime/kpdhj/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.freetime.kpdhj.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.freetime.kpdhj.databinding.FragmentHomeBinding;
+import com.freetime.kpdhj.ui.character.RumiActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -24,6 +28,13 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        Button buttonGetObtainium = binding.buttonGetObtainium;
+        buttonGetObtainium.setOnClickListener(v -> {
+            String url = "https://github.com/imranr98/obtainium"; // URL of the GitHub repository of Obtainium
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
 
         return root;
     }
