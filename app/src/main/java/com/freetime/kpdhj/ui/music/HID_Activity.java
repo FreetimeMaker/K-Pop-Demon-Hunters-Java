ec78faa9d00a864a7feb93cf940e31398c930018
Freetime Maker <jamieachatz@gmail.com>
2025-11-08 17:19:48 +0100

    Changed A Little Bit Again

---
+++ app/src/main/java/com/freetime/kpdhj/ui/music/HID_Activity.java
@@ -0,0 +1,36 @@
+package com.freetime.kpdhj.ui.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.freetime.kpdhj.MainActivity;
+import com.freetime.kpdhj.R;
+
+
+public class HID_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hid);
+
+        // Immersive fullscreen
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+
+        Button BackToMFButton = findViewById(R.id.BackToMFButton);
+        BackToMFButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HID_Activity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
