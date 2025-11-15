fd214362d1f024b11977deffb65c87069357d0bf
Freetime Maker <jamieachatz@gmail.com>
2025-11-15 15:35:43 +0100

    Add MiraActivity and layout; update versionCode and versionName

---
+++ app/src/main/java/com/freetime/kpdhj/ui/character/MiraActivity.java
@@ -0,0 +1,42 @@
+package com.freetime.kpdhj.ui.character;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.freetime.kpdhj.MainActivity;
+import com.freetime.kpdhj.R;
+import com.freetime.kpdhj.ui.music.ZoeyMusicActivity;
+
+public class MiraActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mira);
+
+        // Immersive fullscreen
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+
+        Button miraMusicButton = findViewById(R.id.miraMusicButton);
+        miraMusicButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MiraActivity.this, ZoeyMusicActivity.class);
+            startActivity(intent);
+        });
+
+        Button miraBackToCharactersButton = findViewById(R.id.miraBackToCharactersButton);
+        miraBackToCharactersButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MiraActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
